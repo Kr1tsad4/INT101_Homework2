eacfc146b88e88b6d6fbc98693465663bcdbaa0b
Kritsada <141848930+Kr1tsad4@users.noreply.github.com>
2023-11-12 21:12:51 +0700

    Update Utilitor.java

--- src/main/java/work01Utilitor/Utilitor.java
+++ src/main/java/work01Utilitor/Utilitor.java
@@ -1,42 +1,42 @@
 package work01Utilitor;
 
-
 public class Utilitor {
-    
+
     //1.2
-    public static String testString(String value){
-        if(value == null){
+    public static String testString(String value) {
+        if (value == null) {
             throw new NullPointerException();
         }
-        if(value.isBlank()){
+        if (value.isBlank()) {
             throw new IllegalArgumentException();
         }
         return value;
 
     }
+
     //1.3
-    public static double testPositive(double value){
-        if(value > 0){
+    public static double testPositive(double value) {
+        if (value > 0) {
             return value;
         }
-        throw new IllegalArgumentException(); 
+        throw new IllegalArgumentException();
     }
+
     //1.4
     public static long computeIsbn10(long isbn10) {
         int sum = 0;
-        long digits ;
-        for (int i = 1; i <= 9; i++) {
-           digits = isbn10 % 10;
-           sum += digits * i;
-           isbn10 /= 10;
+        long digits;
+        for (int i = 1; i <= 10; i++) {
+            digits = isbn10 % 10;
+            sum += digits * i;
+            isbn10 /= 10;
         }
-       int checkDigit = sum % 11;
-       
-       return checkDigit;
+        long checkDigit = sum % 11;
+        if (checkDigit == 10) {
+            return 0;
+        }
+        return checkDigit;
     }
 }
 
-    /*
-       1.5 Show in this "work01Utilitor" method how to use 1.2-1.4 and
-           check their correctness. */
 
